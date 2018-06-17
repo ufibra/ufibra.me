6b6dcf79cfa145ebf226b21ce31eb8845ee9bc03
ezradiniz <linux.esdras@gmail.com>
2018-06-16 23:43:11 -0300

    Fix navigations indentation

--- client/src/components/navigations/TopNavigation/TopNavigation.js
+++ client/src/components/navigations/TopNavigation/TopNavigation.js
@@ -5,11 +5,11 @@ import { Col } from 'react-bootstrap';
 import './style.css';
 
 const TopNavigation = () => (
-    <Col md={12} className="ufibra-topNav">
-      <Link to='/' className='ufibra-logo'>
-        <img width={180} src='images/ufibra.png' alt='ufibra.me'/>
-      </Link>
-    </Col>
+  <Col md={12} className="ufibra-topNav">
+    <Link to='/' className='ufibra-logo'>
+      <img width={180} src='images/ufibra.png' alt='ufibra.me'/>
+    </Link>
+  </Col>
 );
 
 export default TopNavigation;
