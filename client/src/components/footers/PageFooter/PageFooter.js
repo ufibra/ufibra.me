251bf9b652c881fe8fbbc7ad3159d1073265529d
ezradiniz <linux.esdras@gmail.com>
2018-06-24 01:25:05 -0300

    Add footer
    Refactor header to home-header

---
+++ client/src/components/footers/PageFooter/PageFooter.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './style.css';
+
+const PageFooter = () => (
+  <footer className='page-footer'>
+    <div className="container">
+      <div className="d-flex justify-content-center">
+	<div className="p-3">
+	  <h5>Comparador</h5>
+	  <p>Plataforma open source para salvar seu treino <i className="fa fa-heart"/></p>
+	</div>
+	<div className="p-3">
+	  <h5>Redes Sociais</h5>
+	  <ul className="nav navbar-nav d-inline-flex">
+	    <li className="nav-item">
+	      <ul className="list-inline-mb-0">
+		<li className="list-inline-item"><a href="#"><i className="fa fa-facebook"/></a></li>
+		<li className="list-inline-item"><a href="#"><i className="fa fa-twitter"/></a></li>
+		<li className="list-inline-item"><a href="#"><i className="fa fa-instagram"/></a></li>
+	      </ul>
+	    </li>
+	  </ul>
+	</div>
+      </div>
+    </div>
+  </footer>
+);
+
+export default PageFooter;
