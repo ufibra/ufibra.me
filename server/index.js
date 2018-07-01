2c36eea837b54c292604faa45fb58168b23fd574
Joao <joaoded@hotmail.com>
2018-06-30 23:19:19 -0300

    Routes file created

--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/api', (req, res) => {
+const apiRoute = require('./routes/routes');
+
+/*app.get('/api', (req, res) => {
   res.json({ express: 'Hello from expresss' });
-});
+});*/
+
+app.use('/products', apiRoute);
 
 app.listen(port, () => console.log(`Running on localhost:${port}`));
