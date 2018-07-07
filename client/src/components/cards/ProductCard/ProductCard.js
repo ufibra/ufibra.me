316d23a7f4b97d32c8751480bf1efe6da8f685c2
ezradiniz <linux.esdras@gmail.com>
2018-07-07 17:06:50 -0300

    Add product cards

---
+++ client/src/components/cards/ProductCard/ProductCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
+
+import "./style.css";
+
+const ProductCard = ({ product }) => (
+  <div className="card product-card">
+    <div className="product-card__image">
+      <img className="card-img-top" src={product.img} alt="" />
+    </div>
+    <div className="card-body">
+      <p className="card-text product-card__price">R$ {product.price}</p>
+      <h5 className="card-title product-card__title">{product.name}</h5>
+      <p className="card-text">{product.description}</p>
+    </div>
+    <div className="card-footer product-card__footer">
+      <Link to={product.href} className="btn product-card__btn">IR À LOJA</Link>
+    </div>
+  </div>
+);
+
+ProductCard.propTypes = {
+  product: PropTypes.shape({}).isRequired
+};
+
+export default ProductCard;
