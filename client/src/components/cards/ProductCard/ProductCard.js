99f692e31d9b445f0584e20c912ca100845b11f0
Joao <joaoded@hotmail.com>
2018-10-14 19:24:26 -0300

    Rendering all whey proteins in the HomePage

--- client/src/components/cards/ProductCard/ProductCard.js
+++ client/src/components/cards/ProductCard/ProductCard.js
@@ -5,18 +5,19 @@ import { Link } from 'react-router-dom'
 import "./style.css";
 
 const ProductCard = ({ product }) => (
-  <div className="card product-card">
-    <div className="product-card__image">
-      <img className="card-img-top" src={product.img} alt="" />
-    </div>
-    <div className="card-body">
-      <p className="card-text product-card__price">R$ {product.price}</p>
-      <h5 className="card-title product-card__title">{product.name}</h5>
-      <p className="card-text">{product.description}</p>
-    </div>
-    <div className="card-footer product-card__footer">
-      <Link to={product.href} className="btn product-card__btn">IR À LOJA</Link><br/>
-      <Link to="/comparisons">Ver preços em mais lojas</Link>
+  <div className="col-4">
+    <div className="card product-card">
+      <div className="product-card__image">
+        <img src={product.image} alt="" />
+      </div>
+      <div className="card-body">
+        <p className="card-text product-card__price">R$ {product.price}</p>
+        <h5 className="card-title product-card__title">{product.name}</h5>
+      </div>
+      <div className="card-footer product-card__footer">
+        <a href={product.url} className="btn product-card__btn">IR À LOJA</a><br/>
+        <Link to="/comparisons">Ver preços em mais lojas</Link>
+      </div>
     </div>
   </div>
 );
