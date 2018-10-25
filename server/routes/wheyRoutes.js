7f2481b99484e6e8a7e5a49d2eb3fa7a22b2f30f
Joao <joaoded@hotmail.com>
2018-10-25 13:51:00 -0300

    Fast refactoring

--- server/routes/wheyRoutes.js
+++ server/routes/wheyRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/wheyprotein');
 
-router.get('/teste', controller.get);
+router.get('/wheyprotein', controller.get);
 
 module.exports = router;
